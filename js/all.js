8479b935cfce4b7a71d6eeb3185834d197ace017
Diego Fortunato <diego.fortunato@mstech.com.br>
2018-09-18 17:12:50 -0300

    refatorado - criado diretorio js

---
+++ js/all.js
@@ -0,0 +1,33 @@
+
+     window.onload = function()
+     {
+     var cidades = new Array ("Bauru","Botucatu","Perdeineiras","Piratininja","Sorocaba","Cafelândia");
+     var comboCidades = document.getElementById("IdCidade");
+
+         for (var x=0; x<cidades.length;x++)
+          {
+            var opt0 = document.createElement("option"); 
+            opt0.value = x;
+            opt0.text =cidades[x];
+            comboCidades.add(opt0, cidades[x]);
+           }
+         }
+        
+        var escola = document.getElementById("IdEscola");
+        
+        /* Pega o evento de click do botão de cadastro
+        var form = document.getElementById("cadastro");
+         form.addEventListener("submit",Cadastrar,false);     */
+         var Cadastrar =function (){
+        /*Verifica o campo da escola*/    
+            if(!escola.value)
+             {
+                 escola.className= "form-control is-invalid";
+                 var aviso = document.getElementById("AvisoEscola");
+                 aviso.className = "alert-danger";  
+                 e.preventdefault();
+             }
+
+             alert("sakjda");
+            
+         }
