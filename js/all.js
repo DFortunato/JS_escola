93dda510dc5502b651744dbcf29804d013784b01
Diego Fortunato <diego.fortunato@mstech.com.br>
2018-09-19 16:30:00 -0300

    listagem ok

--- js/all.js
+++ js/all.js
@@ -1,20 +1,26 @@
-    
+document.write('<script src="js\/dados.js" type="text\/javascript"><\/script>');    
+
 window.onload = function()
 {
-   
-    var cidades = new Array ("Bauru","Botucatu","Perdeineiras","Piratininja","Sorocaba","Cafelândia");
-        var comboCidades = document.getElementById("IdCidade");
-    for (var x=0; x<cidades.length;x++)
-     {
-       var opt0 = document.createElement("option"); 
-       opt0.value = x;
-       opt0.text =cidades[x];
-       comboCidades.add(opt0, cidades[x]);
-      }
+    listarCidades();
     
 };
 
-<<<<<<< HEAD
+function listarCidades(){
+    var cidades = new Array ("Bauru","Botucatu","Perdeineiras","Piratininja","Sorocaba","Cafelândia");
+    var comboCidades = document.getElementById("IdCidade");
+    for (var x=cidades.length-1; x>-1;x--)
+    {
+      var opt0 = document.createElement("option"); 
+      opt0.value = x;
+      opt0.text =cidades[x];
+      comboCidades.add(opt0, cidades[x]);
+     }
+     return cidades;
+
+}
+
+
  function Cadastrar()
    {
        var escola = document.getElementById("IdEscola");
@@ -27,40 +33,11 @@ window.onload = function()
     }
     else
     {
-        window.location.href = "file:///C:/Users/angelo.neto/JS_escola/TelaCadastro.html";
+        Adicionar();
+        window.location.href = "TelaCadastro.html";
     } 
    }
+
   
 
          
-=======
-         for (var x=0; x<cidades.length;x++)
-          {
-            var opt0 = document.createElement("option"); 
-            opt0.value = x;
-            opt0.text =cidades[x];
-            comboCidades.add(opt0, cidades[x]);
-           }
-         }
-        
-        var escola = document.getElementById("IdEscola");
-        
-        /* Pega o evento de click do botão de cadastro
-        var form = document.getElementById("cadastro");
-         form.addEventListener("submit",Cadastrar,false);     */
-          Cadastrar = function (){
-        /*Verifica o campo da escola*/    
-            if(!escola.value)
-             {
-                 escola.className= "form-control is-invalid";
-                 var aviso = document.getElementById("AvisoEscola");
-                 aviso.className = "alert-danger";  
-                 e.preventdefault();
-             }
-
-             alert("testando");
-         }
-         
-
-
->>>>>>> de7c450f9b36a126bfe34e9893ca729c57347d14
--- js/dados.js
+++ js/dados.js
@@ -1,12 +1,74 @@
-var dados = {id: "", escola: "", cidade: ""}
-         var i = 0;
-         
+ 
 
-         _addCidadeEscola = function() {
-            var y = document.getElementById("IdEscola").value;
-            var x = document.getElementById("IdCidade").value;
-            
-            dados[i++] =  {id: i, escola: y, cidade: x};
+var LSEscola = localStorage.getItem("LSEscola");// Recupera os dados armazenados
+LSEscola = JSON.parse(LSEscola); // Converte string para objeto
+if(LSEscola == null) // Caso não haja conteúdo, iniciamos um vetor vazio
 
-            /*var x = document.createTextNode(dados[i].escola);*/
-        }
+LSEscola = [];
+
+Adicionar = function (){
+    var escola = JSON.stringify({
+        ID : LSEscola.length + 1,
+        Escola : document.getElementById("IdEscola").value,
+        Cidade : document.getElementById("IdCidade").value,
+    });
+    LSEscola.push(escola);
+    localStorage.setItem("LSEscola", JSON.stringify(LSEscola));
+    alert("Registro adicionado.");
+    return true;
+}
+
+Editar = function(){
+    LSEscola[_id] = JSON.stringify({
+        ID : _id,
+        Escola : document.getElementById("IdEscola").value,
+        cidade : document.getElementById("IdCidade").value,
+    })
+
+    localStorage.setItem("LSEscola", JSON.stringify(LSEscola));
+    alert("Informações editadas");
+    operacao = "A";
+    return true;
+}
+ 
+Listar = function(){
+    
+    var tbl     = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+
+    for (var i = 0; i < LSEscola.length; i++) {
+        var row = document.createElement("tr");
+        var cell = document.createElement("td");
+        var _result = JSON.parse(LSEscola[i]);
+
+        var cellText = document.createTextNode(_result.Escola);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+
+        cell = document.createElement("td");
+        cellText = document.createTextNode(_result.cidade);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+
+        cell = document.createElement("td");
+        var btnEditar = document.createElement("BUTTON");
+        var txtEditar = document.createTextNode("Editar");
+        btnEditar.appendChild(txtEditar)
+        cell.appendChild(btnEditar);
+
+        row.appendChild(cell);
+
+        cell = document.createElement("td");
+        var btnExcluir = document.createElement("BUTTON");
+        var txtExcluir = document.createTextNode("Excluir");
+        btnExcluir.appendChild(txtExcluir)
+        cell.appendChild(btnExcluir);
+
+        row.appendChild(cell);
+        tblBody.appendChild(row);
+    }
+
+    tbl.appendChild(tblBody);
+    document.body.appendChild(tbl);
+}
+
