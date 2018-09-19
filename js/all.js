9c52c3cd5ba9df0fca6382ed5da264137e38524b
Angelo Rocha <angelo.neto@mstech.com.br>
2018-09-19 12:30:24 -0300

    Seilá mesmo

--- js/all.js
+++ js/all.js
@@ -1,33 +1,34 @@
-
-     window.onload = function()
+    
+window.onload = function()
+{
+   
+    var cidades = new Array ("Bauru","Botucatu","Perdeineiras","Piratininja","Sorocaba","Cafelândia");
+        var comboCidades = document.getElementById("IdCidade");
+    for (var x=0; x<cidades.length;x++)
      {
-     var cidades = new Array ("Bauru","Botucatu","Perdeineiras","Piratininja","Sorocaba","Cafelândia");
-     var comboCidades = document.getElementById("IdCidade");
+       var opt0 = document.createElement("option"); 
+       opt0.value = x;
+       opt0.text =cidades[x];
+       comboCidades.add(opt0, cidades[x]);
+      }
+    
+};
 
-         for (var x=0; x<cidades.length;x++)
-          {
-            var opt0 = document.createElement("option"); 
-            opt0.value = x;
-            opt0.text =cidades[x];
-            comboCidades.add(opt0, cidades[x]);
-           }
-         }
-        
-        var escola = document.getElementById("IdEscola");
-        
-        /* Pega o evento de click do botão de cadastro
-        var form = document.getElementById("cadastro");
-         form.addEventListener("submit",Cadastrar,false);     */
-         var Cadastrar =function (){
-        /*Verifica o campo da escola*/    
-            if(!escola.value)
-             {
-                 escola.className= "form-control is-invalid";
-                 var aviso = document.getElementById("AvisoEscola");
-                 aviso.className = "alert-danger";  
-                 e.preventdefault();
-             }
+ function Cadastrar()
+   {
+       var escola = document.getElementById("IdEscola");
+      
+    if(!escola.value)
+    {
+        escola.className= "form-control is-invalid";
+        var aviso = document.getElementById('AvisoEscola');
+        aviso.className = "alert-danger";
+    }
+    else
+    {
+        window.location.href = "file:///C:/Users/angelo.neto/JS_escola/TelaCadastro.html";
+    } 
+   }
+  
 
-             alert("sakjda");
-            
-         }
+         
