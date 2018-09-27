81d310eecaa96a0995fd5eb0f4c481acd0cf7cfa
Diego Fortunato <diego.fortunato@mstech.com.br>
2018-09-27 10:00:25 -0300

    Finalizado o menun  de cadastro com animação usando Jquery

--- js/lista.js
+++ js/lista.js
@@ -1,7 +1,21 @@
-document.write('<script src="js\/dados.js" type="text\/javascript"><\/script>');    
-window.onload = function()
-{
-    
+document.write('<script src="js\/dados.js" type="text\/javascript"><\/script>');
+window.onload = function () {
+
 };
 
+$(document).ready(function () {
+
+    setInterval(function () {
+        if ($("#img3").attr("src") == "logo.png")
+        {
+            $("#img3").attr("src", "mais facil.jpg");
+        }
+        else
+        {
+            $("#img3").attr("src", "logo.png");
+        }
+    }, 4000);
+
+
+});
 
